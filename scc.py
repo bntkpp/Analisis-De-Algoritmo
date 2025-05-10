d3ce91063365318cbe15a2d2caf83c34762fb3b6
bntkpp <thewild81@gmail.com>
2025-05-09 22:22:59 -0400

    ULTIMOS COMENTARIOS

--- scc.py
+++ scc.py
@@ -11,12 +11,12 @@ def scc_iterativo(x, h):
     N = N1 + N2 - 1 # Longitud de la señal de salida y[n]
 
     # Zero-padding de las señales x[n] y h[n] (UTILIZAR NUMPY)  
-    x = np.pad(x, (0, N - N1), mode='constant') #Los 0 que faltan en N1 para que sean N
-    h = np.pad(h, (0, N - N2), mode='constant') #Los 0 que faltan en N2 para que sean N
+    x = np.pad(x, (0, N - N1), mode='constant') # Los 0 que faltan en N1 para que sean N
+    h = np.pad(h, (0, N - N2), mode='constant') # Los 0 que faltan en N2 para que sean N
 
     y = np.zeros(N) # Inicializar la señal de salida y[n] con ceros
 
-    H = toeplitz(h, h)  #Primer Parametro es la primera columna y el segundo la primera fila
+    H = toeplitz(h, h)  # Primer Parametro es la primera columna y el segundo la primera fila
     
     Ht = np.transpose(H) # Transponer la matriz H para obtener Ht
 
@@ -29,7 +29,8 @@ def scc_iterativo(x, h):
 
 def fft(x):
     N = len(x)
-    if N == 1:
+
+    if N == 1: # Caso base
         return x
     
     # Dividir en subproblemas de tamaño N/2
@@ -40,12 +41,14 @@ def fft(x):
 
     X = [pares[k] + W[k] * impares[k] for k in range(N // 2)] + [pares[k] - W[k] * impares[k] for k in range(N // 2)]  #Combina la mitad inferior y superior de la FFT
 
-    return  np.array(X) # Devuelve la FFT como un array de numpy (Para poder hacer operaciones con ella)
+    return np.array(X) # Devuelve la FFT como un array de numpy (Para poder hacer operaciones con ella)
     
 def ifft(X):
     N = len(X)
+
     if N == 1:
         return X
+    
     # Dividir en subproblemas de tamaño N/2
     pares = ifft(X[::2]) # Elementos en posiciones pares (x[0], x[2], x[4], ...)
     impares = ifft(X[1::2]) # Elementos en posiciones impares (x[1], x[3], x[5], ...)
@@ -62,17 +65,17 @@ def scc_fft(x, h):
     N1 = len(x)
     N2 = len(h)
     N = N1 + N2 - 1 # Longitud de la señal de salida y[n]
-    N_potencia2 = 1 << (N - 1).bit_length()  # siguiente potencia de 2
+    N_potencia2 = 1 << (N - 1).bit_length()  # Siguiente potencia de 2
 
     x = np.pad(x, (0, N_potencia2 - N1), mode='constant')
     h = np.pad(h, (0, N_potencia2 - N2), mode='constant')
 
-    X = fft(x)
-    H = fft(h)
-    Y = X * H
-    y = ifft(Y)
+    X = fft(x) # Transformada de Fourier de x[n]
+    H = fft(h) # Transformada de Fourier de h[n]
+    Y = X * H # Multiplicación de las transformadas de Fourier
+    y = ifft(Y) # Transformada inversa de Fourier
 
-    return np.real_if_close(y[:N])
+    return np.real_if_close(y[:N]) # Devuelve la parte real de la señal de salida y[n] (ya que es real)
 
 
 tiempo_iterativo = [] # Lista para almacenar los tiempos de ejecución del método iterativo
@@ -81,8 +84,8 @@ N = [2**i for i in range(1, 14)] # Lista de longitudes de señales a probar [2, 
 
 
 for n in N:
-    x = np.random.uniform(-1, 1, n)
-    h = np.random.uniform(-1, 1, n)
+    x = np.random.uniform(-1, 1, n) # Generar una señal aleatoria x[n]
+    h = np.random.uniform(-1, 1, n) # Generar una señal aleatoria h[n]
 
     s = process_time()
     y1 = scc_iterativo(x, h)
@@ -96,7 +99,7 @@ for n in N:
 
 
 
-# Graficar
+# Graficar Iterativo vs FFT
 plt.plot(N, tiempo_iterativo, label='Iterativo', marker='o')
 plt.plot(N, tiempo_fft, label='FFT', marker='x')
 plt.xlabel("Tamaño N")
